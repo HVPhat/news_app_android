3af27cb6b5c54df85dc2a1730076c371f590630d
HVPhat <0306181349@caothang.edu.vn>
2020-12-06 18:39:44 +0700

    Lay du lieu bai viet tu localhost

--- app/src/main/java/com/example/news_app/HomeFragment.java
+++ app/src/main/java/com/example/news_app/HomeFragment.java
@@ -1,7 +1,9 @@
 package com.example.news_app;
 
 import android.content.Context;
+import android.os.AsyncTask;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -13,42 +15,91 @@ import androidx.fragment.app.Fragment;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import org.json.JSONArray;
+import org.json.JSONObject;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.URL;
+import java.net.URLConnection;
 import java.util.LinkedList;
 
 public class HomeFragment extends Fragment {
-    LinkedList<BaiViet> wordList;
+    LinkedList<BaiViet> lisBaiViet=new LinkedList<BaiViet>();
     int img = R.drawable.no_image;
     RecyclerView recyclerViewWordList;
     HomeRecyclerViewAdapter homeRecyclerViewAdapter;
+    View rootView;
+    public static final String URL="https://10.0.2.2:8000/api/baiviet/";
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        View rootView = inflater.inflate(R.layout.fragment_home, container, false);
-        //Tạo nguồn dữ liệu
-        this.wordList = new LinkedList<BaiViet>();
-        for(int i=0; i<=50; i++)
-            this.wordList.add(new BaiViet("Bài viết số "+i,"Nội dung bài viết số "+i));
-
+        rootView = inflater.inflate(R.layout.fragment_home, container, false);
+        new KetNoi().execute(URL);
+        if(lisBaiViet!=null){
         //Thiết lập Adapter và LayoutManager cho RecyclerView
         recyclerViewWordList = (RecyclerView) rootView.findViewById(R.id.homeRecyclerView);
-        homeRecyclerViewAdapter = new HomeRecyclerViewAdapter(this.wordList, this.img);
-        recyclerViewWordList.setAdapter(homeRecyclerViewAdapter);
-        recyclerViewWordList.setLayoutManager(new LinearLayoutManager(getActivity()));
+        }
         return rootView;
     }
 
-    /*@Override
-    public void onCreate(@Nullable Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        //Tạo nguồn dữ liệu
-        this.wordList = new LinkedList<String>();
-        for(int i=0; i<=50; i++)
-            this.wordList.add("Word "+i);
 
-        //Thiết lập Adapter và LayoutManager cho RecyclerView
-        recyclerViewWordList = (RecyclerView) findViewById(R.id.homeRecyclerView);
-        homeRecyclerViewAdapter = new HomeRecyclerViewAdapter(this.wordList);
-        recyclerViewWordList.setAdapter(homeRecyclerViewAdapter);
-        recyclerViewWordList.setLayoutManager(new LinearLayoutManager(getActivity()));
-    }*/
+    class KetNoi extends AsyncTask<String,Void,String>{
+
+        @Override
+        protected String doInBackground(String... strings) {
+            return Doc_noi_dung_url("http://10.0.2.2:8000/api/baiviet");
+        }
+
+        @Override
+        protected void onPostExecute(String s) {
+            super.onPostExecute(s);
+            try {
+                Log.d("S", s);
+                JSONObject jsonObject = new JSONObject(s);
+                JSONArray jsonArrayItems=jsonObject.getJSONArray("baiViet");
+                String tieuDe;
+                String noiDung;
+                for(int i=0;i<jsonArrayItems.length();i++){
+                    JSONObject jsonObjectItem = jsonArrayItems.getJSONObject(i);
+                        tieuDe=jsonObjectItem.getString("tieu_de");
+                        Log.d("TIEU_DE",jsonObjectItem.getString("tieu_de"));
+                        noiDung = jsonObjectItem.getString("noi_dung");
+                        Log.d("NOI_DUNG",noiDung);
+                        lisBaiViet.add(new BaiViet(tieuDe,noiDung));
+                }
+                homeRecyclerViewAdapter = new HomeRecyclerViewAdapter(lisBaiViet, img);
+                recyclerViewWordList.setAdapter(homeRecyclerViewAdapter);
+                recyclerViewWordList.setLayoutManager(new LinearLayoutManager(getActivity()));
+            }catch (Exception e){
+                e.printStackTrace();
+            }
+        }
+    }
+
+    public static String Doc_noi_dung_url(String urls) {
+        StringBuilder builder = new StringBuilder();
+        String line;
+        try {
+            //create object url
+            java.net.URL url=new URL(urls);
+            //create urlconnection
+            URLConnection urlConnection=url.openConnection();
+            BufferedReader bufferedReader=new BufferedReader(new InputStreamReader(urlConnection.getInputStream()));
+            while ((line = bufferedReader.readLine()) != null) {
+                builder.append(line + "\n");
+
+            }
+            bufferedReader.close();
+            if (builder.length() == 0) {
+                return null;
+            }
+        } catch (IOException e) {
+            return null;
+        }
+        Log.d("LOG_TAG", builder.toString());
+        return builder.toString();
+    }
+
 }
---
+++ app/src/main/java/com/example/news_app/NetworkUtils.java
@@ -0,0 +1,80 @@
+package com.example.news_app;
+
+import android.util.Log;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.ProtocolException;
+import java.net.URL;
+import java.net.URLConnection;
+
+public class NetworkUtils {
+    public static String GetBaiVietInfo(String Link){
+        HttpURLConnection urlConnection = null;
+        BufferedReader reader = null;
+        String bookJSONString = null;
+        try {
+            URL requestURL = new URL(Link);
+            urlConnection = (HttpURLConnection) requestURL.openConnection();
+            urlConnection.connect();
+            // Get the InputStream.
+            InputStream inputStream = urlConnection.getInputStream();
+            // Create a buffered reader from that input stream.
+            reader = new BufferedReader(new InputStreamReader(inputStream));
+            // Use a StringBuilder to hold the incoming response.
+            StringBuilder builder = new StringBuilder();
+            String line;
+            while ((line = reader.readLine())!=null){
+                builder.append(line);
+                // Since it's JSON, adding a newline isn't necessary (it won't
+                // affect parsing) but it does make debugging a *lot* easier
+                // if you print out the completed buffer for debugging.
+                builder.append("\n");
+
+            }
+            if(builder.length() == 0){
+                return null;
+            }
+            bookJSONString = builder.toString();
+        }catch (IOException e){
+            e.printStackTrace();
+        }finally {
+            if(urlConnection!=null)
+                urlConnection.disconnect();
+            if(reader!=null)
+                try {
+                    reader.close();
+                }catch (IOException e){
+                    e.printStackTrace();
+                }
+        }
+
+        return bookJSONString;
+        /*Log.d("NETWORK",URL);
+        StringBuilder builder = new StringBuilder();
+        String line;
+        try {
+            //create object url
+            java.net.URL url=new URL(URL);
+            //create urlconnection
+            URLConnection urlConnection= (HttpURLConnection) url.openConnection();
+            BufferedReader bufferedReader=new BufferedReader(new InputStreamReader(urlConnection.getInputStream()));
+
+            while ((line = bufferedReader.readLine()) != null) {
+                builder.append(line + "\n");
+
+            }
+            bufferedReader.close();
+            if (builder.length() == 0) {
+                return null;
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }*/
+        //Log.d("LOG_TAG",builder.toString());
+    }
+}
